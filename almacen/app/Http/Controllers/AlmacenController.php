7f80fa7829cdc3fbf0f23c1beddbc68dcfade8b6
estefaniamun <estefaniamunca@gmail.com>
2023-03-08 17:49:48 +0100

    .

--- almacen/app/Http/Controllers/AlmacenController.php
+++ almacen/app/Http/Controllers/AlmacenController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 use App\Models\Almacen;
 
+use App\Models\Producto;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AlmacenController extends Controller
 {
     public function index(){
-        return view('almacenes.index');
+        $producto = Producto::find(Auth::id());
+        return view('almacenes.index')->width('almacen', $producto->almacen()->get());
     }
 
 
